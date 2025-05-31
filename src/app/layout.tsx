0e11064be986d822818ff16f6b35d22f11d7017e
AKANDE Ayobami <83408171+adeyemimichael@users.noreply.github.com>
2025-05-31 23:44:43 +0100

    Add Custom Cursor and Scroll Features

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css'; 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import CustomCursor from "@/components/ui/CustomCursor";
+import ScrollToTop from "@/components/ui/ScrollToTop";
 
 export const metadata: Metadata = {
   title: "EduFounder",
@@ -25,21 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        className=""
-      >
-        {/* Navbar appears globally on every page */}
-           <Navbar />  
-           <main>{children}</main>
-      <Footer></Footer>
+      <body className="">
+        <CustomCursor />
+        <ScrollToTop />
+        <Navbar />  
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
 }
-
-
-
-
-
-
